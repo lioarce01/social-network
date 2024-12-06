c4761ff7c1cd3b44c794ab8dbe942ada987493d2
lioarce01 <lioarce1@gmail.com>
2024-12-06 18:50:35 -0300

    IMPL/User repository

--- src/Domain/Repositories/UserRepository.ts
+++ src/Domain/Repositories/UserRepository.ts
@@ -2,4 +2,10 @@ import { User } from "@prisma/client";
 
 export interface UserRepository {
   getAllUsers(offset?: number, limit?: number): Promise<User[] | null>;
+  getUserById(id: string): Promise<User | null>;
+  updateUser(userData: Partial<User>): Promise<User | null>;
+  deleteUser(id: String): Promise<void>;
+  createUser(userData: Partial<User>): Promise<User | null>;
+  disableUser(id: String): Promise<void>;
+  switchUserRole(id: String): Promise<void>
 }
