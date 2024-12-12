6166cccdb724ed72de2393c53c381900802fd198
lioarce01 <lioarce1@gmail.com>
2024-12-11 21:53:06 -0300

    IMPL/Post Repository

--- src/Domain/Repositories/PostRepository.ts
+++ src/Domain/Repositories/PostRepository.ts
@@ -1,1 +1,16 @@
 import { Prisma, Post } from "@prisma/client";
+
+export interface PostRepository {
+  getAllPosts(offset?: number, limit?: number): Promise<Post[] | null>;
+  getPostById(id: string): Promise<Post | null>;
+  getUserPosts(id: string): Promise<Post[] | null>;
+  createPost(
+    id: string,
+    postData: Prisma.PostCreateInput,
+  ): Promise<{ message: string; post: Post }>;
+  deletePost(id: string): Promise<{ message: string }>;
+  updatePost(
+    id: string,
+    postData: Prisma.PostUpdateInput,
+  ): Promise<{ message: string; post: Post }>;
+}
