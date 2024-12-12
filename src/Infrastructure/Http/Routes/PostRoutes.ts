9a809a0619e131ea05a2830fb5327d7f2e37a1d9
lioarce01 <lioarce1@gmail.com>
2024-12-11 23:04:41 -0300

    IMPL/ Post Routes

---
+++ src/Infrastructure/Http/Routes/PostRoutes.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import express from "express";
+import { container } from "tsyringe";
+import { PostController } from "../Controllers/Post";
+import { setupContainer } from "../../DI/Container";
+
+setupContainer();
+
+const router = express.Router();
+
+const postController = container.resolve(PostController);
+
+router.get("/", (req, res, next) => postController.getAllPosts(req, res, next));
+router.get("/:id", (req, res, next) =>
+  postController.getPostById(req, res, next),
+);
+router.get("/posts/:id", (req, res, next) =>
+  postController.getUserPosts(req, res, next),
+);
+router.post("/", (req, res, next) => postController.createPost(req, res, next));
+router.delete("/id", (req, res, next) =>
+  postController.deletePost(req, res, next),
+);
+router.put("/:id", (req, res, next) =>
+  postController.updatePost(req, res, next),
+);
+
+export default router;
